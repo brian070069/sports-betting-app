2a584397dfa0007dec0505e4f624e33c55e2a5ac
brian <brianwgatundu@gmail.com>
2024-02-13 16:19:19 +0300

    update:baseUrls

--- src/Services/BaseUrls.ts
+++ src/Services/BaseUrls.ts
@@ -1,3 +1,4 @@
-export const fixturesUrl = "https://brian070069.github.io/apis/fixtures.json";
+export const fixturesUrl =
+  "https://brian070069.github.io/json-files/fixtures.json";
 
 // export const fixturesUrl = "http://localhost:3000/fixtuers";
